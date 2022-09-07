106d571eee964adebf832709dba3dc8a0e5a4cfc
Markus Rahkola <markus.m.rahkola@aalto.fi>
2022-09-07 19:18:30 +0300

    harjoittelua

--- harjoittelua/part1/src/App.js
+++ harjoittelua/part1/src/App.js
@@ -1,22 +1,52 @@
+import { useState } from 'react'
 
-const Hello = (props) => {
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
   return (
     <div>
-      <p>Hello {props.name}, your are {props.age} years old</p>
+      button press history: {props.allClicks.join(' ')}
     </div>
   )
 }
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
 const App = () => {
-  const nimi = 'Pekka'
-  const ika = 10
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26+10} />
-      <Hello name="Pekka" age={ika}/>
+      <div>
+        {left}
+        <Button handleClick={handleLeftClick} text='left' />
+        <Button handleClick={handleRightClick} text='right' />
+        {right}
+        <History allClicks={allClicks} />
+      </div>
     </div>
   )
 }
 
-export default App;
+export default App
