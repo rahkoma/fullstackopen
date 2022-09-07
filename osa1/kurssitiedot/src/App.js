d054dc95b748c0cb49270bda69bc0ca22e88ebed
Markus <markus.m.rahkola@aalto.fi>
2022-09-07 14:55:47 +0300

    step3

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,5 +1,6 @@
 
 const Header = (props) => {
+  console.log(props)
   return(
   <div>
       <h1>
@@ -30,7 +31,7 @@ const Total = (props) => {
   return(
     <div>
       <p>
-        Number of exercises {props.exercises}
+        Number of exercises {parts}
       </p>
     </div>
   )
@@ -39,18 +40,26 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+ {
+    name: 'Fundamentals of React',
+    exercises: 10
+  },
+  {
+    name: 'Using props to pass data',
+    exercises: 7
+  },
+  {
+    name: 'State of a component',
+    exercises: 14
+  }
+]
 
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
-      <Total exercises={exercises1 + exercises2 + exercises3}/>
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
