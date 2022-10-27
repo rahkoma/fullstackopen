45c8b311d5934d1c7e23b9e97852ed391244fe04
Markus Rahkola <markus.m.rahkola@aalto.fi>
2022-10-27 16:52:14 +0300

    maiden_tiedot valmis

--- osa2/maiden_tiedot/src/App.js
+++ osa2/maiden_tiedot/src/App.js
@@ -1,44 +1,33 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-import {Filter, Countries, Weather} from './Components/Components.js'
+import {Filter, Countries} from './Components/Components.js'
 
 const App = () => {
   const [countries, setCountries] = useState([])
   const [newFilter, setNewFilter] = useState('')
-  const [weather, setWeather] = useState([])
+  const countriesToShow = countries.filter(country => country.name.common.toLowerCase().includes(newFilter.toLowerCase()))
 
   useEffect(() => {
     axios
     .get('https://restcountries.com/v3.1/all')
     .then(response => {
+      console.log(response.data)
       setCountries(response.data)
     })
   }, [])
-
-  useEffect(() => {
-    axios
-    .get('https://api.open-meteo.com/v1/forecast')
-    .then(response => {
-      setWeather(response.data)
-    })
-  }, [])
-
-  const setCapital = Countries.capital
-  console.log((setCapital))
-
+  
   const handleFilterChange = (event) => {
     setNewFilter(event.target.value)
   }
+
   const handleClick = (event) => {
     setNewFilter(event.target.value)
   }
-  const countriesToShow = countries.filter(country => country.name.common.toLowerCase().includes(newFilter.toLowerCase()))
-
-  return (
+ 
+    return ( 
     <>
      <Filter newFilter={newFilter} handleFilter={handleFilterChange} />
      <Countries countries={countriesToShow} handleClick={handleClick} />
-     <Weather weather={weather} />
     </>
     
   );
--- osa2/maiden_tiedot/src/Components/Components.js
+++ osa2/maiden_tiedot/src/Components/Components.js
@@ -1,6 +1,8 @@
-import {Parsedata} from './Parsedata.js'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
-const Filter = ({newFilter, handleFilter}) =>
+const Filter = ({newFilter, handleFilter}) => {
+  return(
     <form>
         <div>find countries 
             <input
@@ -9,15 +11,34 @@ const Filter = ({newFilter, handleFilter}) =>
             />
         </div>   
     </form>
-  
-const Weather = ({weather}) => {
-  console.log(weather)
+)}
+const Weather = ({capital}) => {
+  const [weather, setWeather] = useState(null)
+  const api_key = process.env.REACT_APP_API_KEY
+
+  useEffect(() => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${api_key}`
+    axios
+    .get(url)
+    .then(response => {
+      setWeather(response.data)
+      console.log(url)
+    })
+  }, [])
+
+
+    if(weather === null) return null
+    else {
+      return(
+        <>
+        <p>temperature {Math.ceil(weather.main.temp - 273.15)} Celcius</p>
+        <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="icon" />
+        <p>wind {weather.wind.speed} m/s</p>
+        </>
+      )}
 }
 
-const Countries = ({countries, handleClick, capital, area, name, flag, language}) => {
-  console.log(countries.length)
-  console.log(countries)
-  if(countries.length === 1) {
+const Country = ({name, capital, area, languages, flag}) => {
     return(
       <>
         <h1>{name}</h1>
@@ -25,25 +46,44 @@ const Countries = ({countries, handleClick, capital, area, name, flag, language}
         <div>area {area}</div>
         <h3>languages:</h3>
         <ul>
-          {Object.values(language).map( language =>
+          {Object.values(languages).map(language =>
             <li key={language}> {language} </li>)}
         </ul>
         <img src={flag} alt="Flag" />
       </>
-    )}
+    )
+}
 
-  else if (countries.length <= 10 && countries.length > 1) {
-    return(
+const Countries = ({countries, handleClick}) => {
+
+  if(countries.length === 1) {
+      return(
       <>
-      {countries.map(country =>
-          <div key={country.name.common}> {country.name.common} <button value={country.name.common} onClick={handleClick}>show</button></div>
-          )}
+          <Country
+          name={countries[0].name.common}
+          capital={countries[0].capital}
+          area={countries[0].area}
+          languages={countries[0].languages}
+          flag={countries[0].flags.png}
+          />
+          
+          <Weather
+          capital={countries[0].capital}
+          />
       </>
-    )}
-  else {
-    return(
-      <p>Too many matches, specify another filter</p>
+      )}
+      else if (countries.length <= 10 && countries.length > 1) {
+          return(
+            <>
+            {countries.map(country =>
+                <div key={country.name.common}> {country.name.common} <button value={country.name.common} onClick={handleClick}>show</button></div>
+                )}
+            </>
+          )}
+      else if(countries.length > 10) {
+          return(
+            <p>Too many matches, specify another filter</p>
       )}
 }
 
-export {Filter, Countries, Weather};
+export {Filter, Countries};
